f7aee8fe0de0b276ceeb37ede2c92424b97d7339
Pavel <ipagar@icloud.com>
2021-10-04 01:28:16 +0300

    add detail properties to RepoItem

--- src/components/RepoItem/RepoItem.tsx
+++ src/components/RepoItem/RepoItem.tsx
@@ -1,35 +1,66 @@
 import React from "react";
+import { LanguageType } from "../../adapters/ReposAdapter";
 import styles from "./RepoItem.module.css";
 
 type RepoItemProps = {
   ownerLogin: string;
   name: string;
-  desccription: string;
-  stargazerCount: number;
+  description?: string;
+  stargazerCount?: number;
   primaryLanguageName: string | null;
   onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
-  props?: unknown;
+  pullRequestsCount?: number;
+  issuesCount?: number;
+  languages?: LanguageType[];
 };
 
 const RepoItem: React.FC<RepoItemProps> = ({
   ownerLogin,
   name,
-  desccription,
+  description,
   stargazerCount,
   primaryLanguageName,
   onClick,
+  pullRequestsCount,
+  issuesCount,
+  languages,
   ...props
 }) => {
   return (
-    <div className={styles.repoItem} onClick={onClick} {...props}>
+    <div
+      className={styles.repoItem}
+      style={{ cursor: onClick ? "pointer" : "default" }}
+      onClick={onClick}
+      {...props}
+    >
       <div className={styles.name}>
         {ownerLogin}/{name}
       </div>
-      <div>{desccription}</div>
+      <div>{description}</div>
       <div className={styles.additional}>
-        {primaryLanguageName && <span>{primaryLanguageName}</span>}
-        <span>{stargazerCount}</span>
+        {primaryLanguageName && (
+          <span>Primary Language: {primaryLanguageName}</span>
+        )}
+        {stargazerCount !== undefined && <span>Stars: {stargazerCount}</span>}
       </div>
+      {pullRequestsCount !== undefined && (
+        <div className={styles.pullRequestsCount}>PR: {pullRequestsCount}</div>
+      )}
+      {issuesCount !== undefined && <div>Issues: {issuesCount}</div>}
+      {languages && languages.length > 0 && (
+        <div>
+          <span>Languages: </span>
+          {languages.map((language, i) => {
+            const { name, color } = language.node;
+            return (
+              <span key={i} style={{ color }}>
+                {name}
+                {i !== languages.length - 1 && ", "}
+              </span>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
