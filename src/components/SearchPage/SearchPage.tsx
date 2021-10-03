9ccd3ff05f48619bf0ff72f7eabf0a95a28239de
Pavel <ipagar@icloud.com>
2021-10-04 00:09:55 +0300

    add SearchPage

---
+++ src/components/SearchPage/SearchPage.tsx
@@ -0,0 +1,66 @@
+import { client, useGetRepos } from "../../adapters/ReposAdapter";
+import styles from "./SearchPage.module.css";
+import RepoItem from "../RepoItem/RepoItem";
+import SearchInput from "../SearchInput/SearchInput";
+import SearchButton from "../SearchButton/SearchButton";
+import { useHistory } from "react-router-dom";
+
+const SearchPage: React.FC = () => {
+  const history = useHistory();
+  const { getRepos, repos, pageInfo, fetchMore } = useGetRepos();
+
+  async function onInputChange(
+    e: React.ChangeEvent<HTMLInputElement>
+  ): Promise<void> {
+    const { value } = e.currentTarget;
+
+    await client.clearStore();
+    getRepos(value);
+  }
+
+  function onSearchButtonClick() {
+    fetchMore();
+  }
+
+  function onRepoItemClick(e: React.MouseEvent<HTMLDivElement>) {
+    const { name, owner } = e.currentTarget.dataset;
+    history.push(`/${name}/${owner}`);
+  }
+
+  return (
+    <div className={styles.searchPage}>
+      <SearchInput
+        placeholder={"Введите название репозитория"}
+        onChange={onInputChange}
+      />
+      {repos?.map((repo, i) => {
+        const ownerLogin = repo.node.owner.login;
+        const {
+          node: { name, description, stargazerCount },
+        } = repo;
+        const primaryLanguageName = repo.node.primaryLanguage?.name || null;
+        return (
+          <RepoItem
+            key={i}
+            data-name={name}
+            data-owner={ownerLogin}
+            ownerLogin={ownerLogin}
+            name={name}
+            desccription={description}
+            stargazerCount={stargazerCount}
+            primaryLanguageName={primaryLanguageName}
+            onClick={onRepoItemClick}
+          />
+        );
+      })}
+      {pageInfo?.hasNextPage && (
+        <SearchButton
+          hasNextPage={pageInfo.hasNextPage}
+          onClick={onSearchButtonClick}
+        />
+      )}
+    </div>
+  );
+};
+
+export default SearchPage;
